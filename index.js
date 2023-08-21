8c4d0c45c8e0ff5732beb4d358b6a8bf9be47868
solidsnakebyte <59436788+solidsnakebyte@users.noreply.github.com>
2023-08-21 12:10:49 -0700

    Bugs fixed! Color added.

--- index.js
+++ index.js
@@ -11,73 +11,88 @@ const doseInput = document.getElementById("dose-input");
 const freqSelector = document.getElementById("frequency-selector");
 const mmeText = document.getElementById("mme-text");
 
-// container for delegation
+// containers
 const medicationContainer = document.getElementById("medication-container");
+const mmeBox = document.getElementById("mme-box");
 
 const medList = [];
-
-let orderNumber;
+let targetInnerHTML = "";
 
 const createOrder = function () {
-
   const medName = medSelector.options[medSelector.selectedIndex].text;
   const dose = Number(doseInput.value);
   const frequency = Number(freqSelector.value);
   const morphineEQ = Number(medSelector.value);
   const mme = dose * frequency * morphineEQ;
-  const medLabel = `MME: ${mme} - ${medName} ${dose}mg every ${24 / frequency} hours.`;
+  const medLabel = `MME: ${mme} - ${medName} ${dose}mg every ${
+    24 / frequency
+  } hours.`;
   medList.push([medName, dose, frequency, mme, medLabel]);
   createDiv();
-  // updateMMEText();
+  updateMMEText();
+  showMMEBox();
 };
 
-const createDiv = function () { 
-  let label = medList[medList.length-1][4];
-  let div = `<div id="medication-${orderNumber}" class="medication">${label}</div>`;
+const createDiv = function () {
+  let label = medList[medList.length - 1][4];
+  let div = `<div class="medication">${label}</div>`;
   medicationContainer.innerHTML += div;
 };
 
 const calcMME = function () {
   let totalMME = 0;
-
   for (const i of medList) {
     totalMME += i[3];
-    console.log(i[3]);
   }
   return totalMME;
 };
 
 const updateMMEText = function () {
-  mmeText.innerText = calcMME();
+  const text = "Total MME: ";
+  mmeText.innerText = text + calcMME();
 };
 
-const removeActiveClass = function () { 
+const removeActiveClass = function () {
   const elements = document.querySelectorAll(".active");
   elements.forEach((element) => {
-    element.classList.remove('active');
+    element.classList.remove("active");
   });
-}
-
-const removeOrder = function () {
-  const activeElement = document.querySelector('.active');
-  const medicationID = activeElement.id;
-  const order = Number(medicationID.charAt(medicationID.length-1));
-  const index = order - 1;
-  console.log(medList[index]);
-  medList.splice(index, 1);
-  calcMME();
-  removeDivs();
-  updateMMEText();
 };
 
+const removeOrder = function () {
+  const activeElement = document.querySelector(".active");
 
-const removeDivs = function () { 
+  // stores string value of item
+  let activeText = activeElement.innerText;
+  console.log("MedList Before: ", medList);
+
+  // scan over the meds and find a match
+  for (const med of medList) {
+    let medLable = med[4];
+
+    if (medLable == activeText) {
+      let index = medList.indexOf(med);
+      medList.splice(index, 1);
+      removeDivs();
+      calcMME();
+      updateMMEText();
+      // stops cycling through if match is found. 
+      break;
+    }
+  }
+};
+
+const showMMEBox = function () {
+  mmeBox.classList.remove("hidden");
+};
+
+const removeDivs = function () {
   const selectedElements = document.querySelectorAll(".active");
   selectedElements.forEach((n) => n.remove());
-}
+};
 
 const reset = function () {
-  medList = [];
+  // medList = [];
   mmeText.innerText = "";
 };
 
@@ -85,10 +100,11 @@ addBtn.addEventListener("click", createOrder);
 removeBtn.addEventListener("click", removeOrder);
 resetBtn.addEventListener("click", reset);
 
-medicationContainer.addEventListener("click", e => {
-    removeActiveClass();
-    if (e.target && e.target.tagName == "DIV") {
-      e.target.classList.toggle("active");
-    }
-  });
-
+medicationContainer.addEventListener("click", (e) => {
+  removeActiveClass();
+  // if (e.target && e.target.tagName == "DIV") {
+  if (e.target && e.target.classList == "medication") {
+    e.target.classList.toggle("active");
+    targetInnerHTML = e.target.innerHTML;
+  }
+});
